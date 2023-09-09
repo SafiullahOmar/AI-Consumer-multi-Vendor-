dd1c4109cd0b0d4bcc493a4653a35684a3db0bf2
Safiullah Omar <omar.safiullah@gmail.com>
2023-09-09 13:44:16 +0200

    defining constructor and Depth value

--- main/serializers.py
+++ main/serializers.py
@@ -1,7 +1,20 @@
 from  rest_framework import serializers
 from . import models
+
+
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model=models.Vendor
-        fields=['user','address']
+        fields=['id','user','address']
+        depth=1
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class VendorDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Vendor
+        fields=['id','user','address']
+        depth=1
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from . import models 
 from . import serializers
-from rest_framework import generics
+from rest_framework import generics ,permissions
 # Create your views here.
 
-class VendorList(generics.ListAPIView):
+class VendorList(generics.ListCreateAPIView):
     queryset=models.Vendor.objects.all()
     serializer_class=serializers.VendorSerializer
+    #permission_classes=[permissions.IsAuthenticated]
+
+class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=models.Vendor.objects.all()
+    serializer_class=serializers.VendorDetailSerializer
+    #permission_classes=[permissions.IsAuthenticated]        
